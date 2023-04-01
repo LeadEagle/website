d13632b7d5b7fd17397a9f5653d26f38d7070079
Adrian Orłów <adrian@orlow.me>
2023-04-01 20:06:26 +0200

    fix: fonts

--- src/layout/Head/HeadFonts.tsx
+++ src/layout/Head/HeadFonts.tsx
@@ -1,17 +1,48 @@
 import { fonts } from '@constants';
-import * as R from 'ramda';
+import Head from 'next/head';
 import * as React from 'react';
 
 const fontsPreloadPaths = fonts.map(({ path }) => path);
 
+const toFontPreloadLink = (path: string) => (
+  <link
+    key={path}
+    rel="preload"
+    as="font"
+    crossOrigin=""
+    type="font/ttf"
+    href={`/static/fonts/${path}`}
+  />
+);
+
+const fontsFacesDeclarations = fonts
+  .map(
+    ({ path, name, weight }) => `
+@font-face {
+  font-family: '${name}';
+  src: url('/static/fonts/${path}');
+  font-style: normal;
+  font-weight: ${weight};
+  font-display: auto;
+}
+`
+  )
+  .join('');
+
+const fontsPreload = fontsPreloadPaths.map(toFontPreloadLink);
+
 const HeadFonts: React.FC = () => {
-  const toFontPreloadLink = (path: string) => (
-    <link key={path} as="font" crossOrigin="" href={`/static/fonts/${path}`} />
+  const [fontsPreloadData] = React.useState(fontsPreload);
+  const [fontsFacesDeclarationsData] = React.useState(fontsFacesDeclarations);
+
+  return (
+    <>
+      <Head>{fontsPreloadData}</Head>
+      <style jsx global>
+        {fontsFacesDeclarationsData}
+      </style>
+    </>
   );
-
-  const fontsPreload = R.map(toFontPreloadLink, fontsPreloadPaths);
-
-  return <>{fontsPreload}</>;
 };
 
-export default HeadFonts;
+export default React.memo(HeadFonts);
