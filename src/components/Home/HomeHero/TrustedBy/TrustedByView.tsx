e69a0a436ddb7e3d710a1f75289c7f9d6354ffd1
Adrian Orłów <adrian@orlow.me>
2023-08-03 22:53:46 +0200

    feat: add new clients

--- src/components/Home/HomeHero/TrustedBy/TrustedByView.tsx
+++ src/components/Home/HomeHero/TrustedBy/TrustedByView.tsx
@@ -15,10 +15,16 @@ const TrustedBy: React.FC = () => {
         <CompanyLink href="https://intertell.pl" target="_blank">
           <CompanyLogo src={require('@assets/images/intertell.png')} />
         </CompanyLink>
-        <CompanyLogo src={require('@assets/images/mermaid.png')} />
+        <CompanyLink href="https://off.org.pl" target="_blank">
+          <CompanyLogo src={require('@assets/images/off.png')} />
+        </CompanyLink>
+        <CompanyLink href="https://liberte.pl" target="_blank">
+          <CompanyLogo src={require('@assets/images/liberte.png')} />
+        </CompanyLink>
         <CompanyLink href="https://steki.pl" target="_blank">
           <CompanyLogo src={require('@assets/images/steki.png')} />
         </CompanyLink>
+        <CompanyLogo src={require('@assets/images/mermaid.png')} />
       </Inner>
     </Container>
   );
